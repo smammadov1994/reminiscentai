76df2c1c20c94be48c5ba87735c78f82cdf076d2
Seymur Mammadov <smammadov494@gmail.com>
2025-10-31 03:16:50 -0400

    Fix base path for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     host: '0.0.0.0',
   },
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/copy-of-logo-reactivator/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/reminiscentai/' : '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, '.'),
